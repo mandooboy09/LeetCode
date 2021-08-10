e5c1b9066111eda42d9df755632e65a68a7e177e
Tae yong Song <mandooboy09@gmail.com>
2021-08-10 12:47:25 +0900

    Update minimum-number-of-operations-to-move-all-balls-to-each-box.java
    O(N^2) -> O(N) 최적화

--- minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -3,15 +3,27 @@ class Solution {
         int n = boxes.length();
         int answer[] = new int[n];
         
+        int leftCount = 0;
+        int leftCost = 0;
+        int rightCount = 0;
+        int rightCost = 0;
         
-        for(int i = 0; i < n; i++){
-            int operationCount = 0;
-            for(int j = 0; j < n; j++){
-                if(i != j && boxes.charAt(j) == '1'){
-                    operationCount += Math.abs(j - i);
-                }
+        //leftCost
+        for(int i = 1; i < n; i++){
+            if(boxes.charAt(i - 1) == '1'){
+                leftCount += 1;
             }
-            answer[i] = operationCount;
+            leftCost += leftCount;
+            answer[i] += leftCost;
+        }
+        
+        //rightCost
+        for(int i = n - 2; i >= 0; i--){
+            if(boxes.charAt(i + 1) == '1'){
+                rightCount += 1;
+            }
+            rightCost += rightCount;
+            answer[i] += rightCost;
         }
         
         return answer;

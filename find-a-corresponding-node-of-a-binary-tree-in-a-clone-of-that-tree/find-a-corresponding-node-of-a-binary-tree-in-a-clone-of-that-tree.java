c1b3a678d887e0e3223412e256ec268a5b52b065
Tae yong Song <mandooboy09@gmail.com>
2021-08-10 14:50:25 +0900

    Time: 0 ms (100.00%), Space: 46.7 MB (47.86%) - LeetHub
    시간단축 반복문 -> 재귀로 변경

--- find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -10,27 +10,24 @@
 
 class Solution {
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        Queue<TreeNode> bfsQueue = new LinkedList<>();
-        bfsQueue.add(cloned);
+        //base case
+        if(target.val == cloned.val){
+            return cloned;
+        }
         
         TreeNode clonedTarget = null;
-        
-        while(!bfsQueue.isEmpty()){
-            TreeNode node = bfsQueue.poll();
-
-            if(node.val == target.val){
-                clonedTarget = node;
-                break;
-            }
-            
-            if(node.left != null){
-                bfsQueue.add(node.left);
-            }
-            
-            if(node.right != null){
-                bfsQueue.add(node.right);
-            }
+        if(cloned.left != null){
+            clonedTarget = getTargetCopy(original, cloned.left, target);
         }
-        return clonedTarget;
+        
+        if(clonedTarget != null){
+            return clonedTarget;
+        }
+
+        if(cloned.right != null){
+            clonedTarget = getTargetCopy(original, cloned.right, target);
+        }        
+        
+        return getTargetCopy(original, cloned.right, target);
     }
 }

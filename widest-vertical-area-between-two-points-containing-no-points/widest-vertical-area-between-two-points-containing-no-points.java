b1dd8566aea713a0b08aa9bf569f742ba543ea64
Tae yong Song <mandooboy09@gmail.com>
2021-08-11 14:27:38 +0900

    Time: 2 ms (85.96%), Space: 39.1 MB MB (61.93%) - LeetHub
    메소드 명 간소화
    최대값인덱스 메소드 모듈화

--- widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.java
+++ widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.java
@@ -1,23 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int maxWidthOfVerticalArea(int[][] points) {
-        int answer = 0;
-        int[] arr = new int[points.length];
-
-        for(int i = 0; i < points.length; i++){
-            arr[i] = points[i][0];
-        }
+    public int getMaxIndex(int[] nums, int begin, int end){
+        int index = begin;
         
-        Arrays.sort(arr);
-        
-        for(int i = 1; i < arr.length; i++){
-            int leftX = arr[i-1];
-            int rightX = arr[i];
-            if(answer < rightX - leftX){
-                answer = rightX - leftX;
+        for(int i = begin; i <= end; i++){
+            if(nums[index] < nums[i]){
+                index = i;
             }
         }
+        return index;
+    }
+    public TreeNode construct(int[] nums, int begin, int end){
+        if(begin > end){
+            return null;
+        }
         
-        return answer;
+        int index = getMaxIndex(nums, begin, end);
+        int val = nums[index];
+                
+        TreeNode node = new TreeNode(val);
+        node.left = construct(nums, begin, index - 1);
+        node.right = construct(nums, index + 1, end);
+
+        return node;
+    }
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        int n = nums.length;
+        int index = getMaxIndex(nums, 0, n - 1);
+        int val = nums[index];
         
+        TreeNode root = new TreeNode(val);
+        root.left = construct(nums, 0, index - 1);
+        root.right = construct(nums, index + 1, n - 1);
+        
+        return root;
     }
 }

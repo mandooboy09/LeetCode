d08db8e0c2cc83f024efef385ee267a293477356
Tae yong Song <mandooboy09@gmail.com>
2021-08-12 15:15:31 +0900

    Time: 1 ms (92.23%), Space: 39.4 MB (40.02%) - LeetHub
    LinkedList -> ArrayList 자료 구조 변경 visited / depth 추가
    
    Time: 3 ms (16.67%), Space: 39.5 MB (27.45%) -> 1 ms (92.23%), Space: 39.4 MB (40.02%) 향상

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,31 +1,33 @@
 class Solution {
-    public void search(int[] nums, LinkedList<Integer> list, List<List<Integer>> answer){
+    public void search(int[] nums, int depth, ArrayList<Integer> list, List<List<Integer>> answer, boolean[] visited){
         //base case
-        if(list.size() == nums.length){
-            answer.add(new LinkedList(list));
+        if(depth == nums.length){
+            answer.add(new ArrayList(list));
             return;
         }
         
         for(int i = 0; i < nums.length; i++){
-            if(list.contains(nums[i])){
+            if(visited[i]){
                 continue;
             }
+            visited[i] = true;
             
             list.add(nums[i]);
-            
-            search(nums, list, answer);
-            
+            search(nums, depth + 1, list, answer, visited);
             list.remove(list.size() - 1);
+            
+            visited[i] = false;
             
         }
         
         return;
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> answer = new LinkedList<>();
-        LinkedList<Integer> list = new LinkedList<>();
+        List<List<Integer>> answer = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
         
-        search(nums, list, answer);
+        search(nums, 0, list, answer, visited);
         
         return answer;
     }

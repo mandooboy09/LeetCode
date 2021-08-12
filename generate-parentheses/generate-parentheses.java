1dbf05b0a243cda65fda01f7aae5ccb1bbb24a71
Tae yong Song <mandooboy09@gmail.com>
2021-08-12 16:46:49 +0900

    Time: 0 ms (100.00%), Space: 39.2 MB (56.06%) - LeetHub
    Time: 8 ms (12.70%), Space: 39.2 MB (43.47%) -> 0 ms (100.00%), Space: 39.2 MB (56.06%)

--- generate-parentheses/generate-parentheses.java
+++ generate-parentheses/generate-parentheses.java
@@ -1,45 +1,33 @@
 class Solution {
-    public static char OPEN = '(';
-    public static char CLOSE = ')';
 
-    public void generateParenthesis(List<String> list, String str, int openCount, int closeCount){
-        if(openCount > closeCount){
+    public void bracket(List<String> arr, StringBuilder str, int open, int close, int max){
+        //base case
+        if(str.length() == max * 2){
+            arr.add(str.toString());
             return;
         }
         
-        if(openCount == 0 && closeCount == 0){
-            list.add(str);
-            return;
-        }
-        
-        if(openCount > 0){
-            str += OPEN;
-            generateParenthesis(list, str, openCount - 1, closeCount);
-            str = str.substring(0, str.length()-1);
+        if(open < max){
+            str.append('(');
+            bracket(arr, str, open + 1, close, max);
+            str.setLength(str.length()-1);
         }
 
-        if(closeCount > 0){
-            str += CLOSE;
-            generateParenthesis(list, str, openCount, closeCount - 1);
-            str = str.substring(0, str.length()-1);
+        if(close < open){
+            str.append(')');
+            bracket(arr, str, open, close + 1, max);
+            str.setLength(str.length()-1);
         }        
         
         return;
     }
     
     public List<String> generateParenthesis(int n) {
-        char[] arr = new char[n * 2];
-        String str = "";
-        boolean[] visited = new boolean[n * 2];
-        for(int i = 0; i < n; i++){
-            arr[i] = '(';
-        }
-        for(int i = n; i < n * 2; i++){
-            arr[i] = ')';
-        }
+        List<String> arr = new ArrayList<>();
         
-        List<String> list = new ArrayList<>();
-        generateParenthesis(list, str, n, n);
-        return list;
+        bracket(arr, new StringBuilder(), 0, 0, n);
+        
+        return arr;
+
     }
 }
